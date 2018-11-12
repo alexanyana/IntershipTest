7ef9d4f3783b894de86cef07f15103bf54afcc7a
alexanyana <yana.miretska.ui.2014@lpnu.ua>
2018-11-12 23:38:10 +0200

    changes of main

--- main.js
+++ main.js
@@ -1,65 +1,86 @@
+var levelStud = [];
 
 
-var level0 = new Knowledge('0');
-var level1 = new Knowledge('1');
-var level2 = new Knowledge('2');
-var level3 = new Knowledge('3');
-var level4 = new Knowledge('4');
+levelStud[0] = new Knowledge('0');
+levelStud[1] = new Knowledge('1');
+levelStud[2] = new Knowledge('2');
+levelStud[3] = new Knowledge('3');
+levelStud[4] = new Knowledge('4');
 
 
-var student1 = new Student('Vasyl Bereza');
-var student2 = new Student('Kolya White');
-var student3 = new Student('Ulyana Yong');
-var student4 = new Student('Mykyta Glad');
-var student5 = new Student('Kateryna Puchyk');
-var student6 = new Student('Nazar Kulya');
+function Middle(arrlevelStud) {
+
+	var sumlevel = 0;
+	for (i = 0; i < arrlevelStud.length; i++) {
+		sumlevel += parseInt(arrlevelStud[i].getLevel());
+	}
+	return parseInt(sumlevel / arrlevelStud.length);
+}
+var middlelevel = Middle(levelStud);
+
+var student = [];
 
 
-student1.setKnowledge();
-student2.setKnowledge(level0);
-student3.setKnowledge(level1);
-student4.setKnowledge(level2);
-student5.setKnowledge(level3);
-student6.setKnowledge(level4);
+function addArrNameFromSomething() {
+	var arrNameOfStudents = [
+    'Vasyl Bereza',
+    'Kolya White',
+    'Ulyana Yong',
+    'Mykyta Glad',
+    'Kateryna Puchyk',
+    'Nazar Kulya'];
+
+
+	for (i = 0; i < arrNameOfStudents.length; i++) {
+		student[i] = new Student(arrNameOfStudents[i]);
+	}
+	return student;
+}
+student = addArrNameFromSomething();
+
+function setRandomKnowledge(arrofStudents, stlevel) {
+	for (i = 0; i < arrofStudents.length; i++) {
+		var lev = Math.floor(Math.random() * stlevel.length);
+		arrofStudents[i].setKnowledge(stlevel[lev]);
+//		console.log(lev + ' ' + arrofStudents[i].getName());
+	}
+}
+
+setRandomKnowledge(student, levelStud);
 
 
 var university = new University('CH.U.I.');
+university.addStudents(student);
 
-university.setArrStud(student1);
-university.setArrStud(student2);
-university.setArrStud(student3);
-university.setArrStud(student4);
-university.setArrStud(student5);
-university.setArrStud(student6);
+
 
 var intership = new Intership('Interlink');
-intership.setArrStud(university.getArrStud());
+intership.setArrStud(university.getArrStud(), middlelevel);
 
 studetsIntership = intership.getArrStud();
 
 var list = "";
-for(var i = 0; i < studetsIntership.length; i++) {
-   list += studetsIntership[i].getName()+ "\n"  ; 
+for (var i = 0; i < studetsIntership.length; i++) {
+	list += studetsIntership[i].getName() + "\n";
 
 }
-console.log(list);
+console.log("Intership list:\n" + list);
 
 
 function Intership(nameInter) {
 
 	var _nameInter = nameInter;
 	var _arrStudInt = [];
-	this.setArrStud = function (arrStudent) {
+	this.setArrStud = function (arrStudent, middlelevel) {
 		var student;
 		for (var i = 0; i < arrStudent.length; i++) {
 			student = arrStudent[i];
 			if (typeof student.getKnowledge() != "undefined") {
-           
-				if (student.getKnowledge().getLevel() >= 2) {
+
+				if (student.getKnowledge().getLevel() > middlelevel) {
 					_arrStudInt.push(student);
 				}
-			}
-		else {
+			} else {
 				console.log(student.getName() + " doesn`t have knowledge");
 			}
 		}
@@ -79,26 +100,25 @@ function Knowledge(level) {
 }
 
 
+function Student(name) {
 
-function Student(name){
-	
 	var _name = name;
 	var _knowledge;
-	
-	this.getName = function(){
+
+	this.getName = function () {
 		return _name;
-	} 
-	this.setKnowledge = function(knowledge){
-		if (knowledge instanceof Knowledge){
-			_knowledge = knowledge;
-			
-		} 
 	}
-	this.getKnowledge = function(){
+	this.setKnowledge = function (knowledge) {
+		if (knowledge instanceof Knowledge) {
+			_knowledge = knowledge;
+
+		}
+	}
+	this.getKnowledge = function () {
 		return _knowledge;
 	}
-} 
-	
+}
+
 function University(nameUni) {
 
 	var _nameUni = nameUni;
@@ -109,6 +129,11 @@ function University(nameUni) {
 			_arrStud.push(student);
 		} else {
 			console.log("Type is wrong");
+		}
+	}
+	this.addStudents = function (arrStudent) {
+		for (i = 0; i < arrStudent.length; i++) {
+			_arrStud.push(arrStudent[i])
 		}
 	}
 
